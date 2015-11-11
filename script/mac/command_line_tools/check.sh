c7c5c2c7bfc56df4cccffa3f76ce133f8631854b
Logan Hasson <logan.hasson@gmail.com>
2015-11-10 21:51:18 -0500

    Quick test

--- script/mac/command_line_tools/check.sh
+++ script/mac/command_line_tools/check.sh
@@ -4,11 +4,13 @@ function are_command_line_tools_installed {
   xcode-select -p &>/dev/null
   rc=$?
 
-  if [[ $rc != 0 ]]; then
-    echo -n '0'
-  else
-    echo -n '1'
-  fi
+  echo -n '7'
+
+  #if [[ $rc != 0 ]]; then
+    #echo -n '0'
+  #else
+    #echo -n '1'
+  #fi
 }
 
 are_command_line_tools_installed
